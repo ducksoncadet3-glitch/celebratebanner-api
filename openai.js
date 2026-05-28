a693c240d77dfeb86e6121d685666e0d1b515774
ducksoncadet3-glitch <ducksoncadet3@gmail.com>
2026-05-28 12:29:21 -0400

    Delete openai.js

--- openai.js
+++
@@ -1,9 +0,0 @@
-const LANGUAGES = [
-  { code: 'en', name: 'English' },
-  { code: 'fr', name: 'French' },
-  { code: 'es', name: 'Spanish' },
-];
-
-const SOURCE_LANGUAGE = 'en';
-
-module.exports = { LANGUAGES, SOURCE_LANGUAGE };
